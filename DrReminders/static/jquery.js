344a1862c40496d18a936b88747e08edfef9b245
dustin-chan-89 <busterdust@gmail.com>
2016-05-24 19:21:30 -0700

    added parralax login template

--- DrReminders/static/jquery.js
+++ DrReminders/static/jquery.js
@@ -1,3 +1,11 @@
-$(document).ready(function() {
-  $("collapse").collapse();
+$(document).ready(function(){
+  $(document).mousemove(function(e){
+     TweenLite.to($('body'),
+        .5,
+        { css:
+            {
+                'background-position':parseInt(event.pageX/8) + "px "+parseInt(event.pageY/12)+"px, "+parseInt(event.pageX/15)+"px "+parseInt(event.pageY/15)+"px, "+parseInt(event.pageX/30)+"px "+parseInt(event.pageY/30)+"px"
+            }
+        });
+  });
 });
